446a19870606be943a6f09344a0c49f0411ed053
Aleksandar Markovic <aleksandar.markovic@imgtec.com>
2017-01-25 11:03:03 +0100

    MIPS: Add Ranchu virtual machine for Mips
    This commit adds Mips Ranchu virtual machine used by Android
    emulator. "Goldfish" and "ranchu" are names for two generations
    of virtual boards used by Android emulator. Ranchu is a newer
    one among the two, and this commit deals with Ranchu. However,
    for historical reasons, some directory, file, device, and variable
    names in this commit still contain the word "goldfish". Such name
    organization is still present in Android code, and this commit
    just reflects it in order to avoid additional differences between
    upstream and Android kernels.
    
    Mips Ranchu machine represents a virtual board which is used by
    Android emulator. It inherits a number of Goldfish devices which
    are modified to be used without the Goldfish platform Bus controller
    and are installed using the Simple Bus interface. The support for
    Virtio devices is also included. Ranchu board supports up to 16
    virtio devices which can be attached using virtio MMIO Bus. This is
    summarized in the following picture:
    
    ABUS
     ||----MIPS CPU
     ||      |                      IRQs
     ||----Goldfish PIC-------------------(32)------
     ||                   | | | | | | | |      | |
     ||----Goldfish TTY---- | | | | | | |      | |
     ||                     | | | | | | |      | |
     ||----Goldfish RTC------ | | | | | |      | |
     ||                       | | | | | |      | |
     ||----Goldfish Timer------ | | | | |      | |
     ||                         | | | | |      | |
     ||----Goldfish FB----------- | | | |      | |
     ||                           | | | |      | |
     ||----Goldfish Events--------- | | |      | |
     ||                             | | |      | |
     ||----Goldfish Audio------------ | |      | |
     ||                               | |      | |
     ||----Goldfish Battery------------ |      | |
     ||                                 |      | |
     ||----Android PIPE------------------      | |
     ||                                        | |
     ||----Virtio MMIO Bus                     | |
     ||      |   |   |                         | |
     ||      |   |   (virtio-block)------------- |
     ||    (16)  |                               |
     ||      |   (virtio-net)---------------------
    
    Device tree support is also contained in this commit. Device Tree is
    created on the QEMU side based on the information about devices IO map
    and IRQ numbers. DTB is subsequently automatically generated and
    appended right after the area where the kernel is loaded. Kernel will
    pick up this DTB and register everything according to the information
    provided. It is assumed that the kernel has following two options emabled:
    CONFIG_MIPS_RAW_APPENDED_DTB and CONFIG_MIPS_DTB_ALTERNATIVE_POSITION,
    Otherwise, it will use the built-in device tree which may hold stale
    information.
    
    Signed-off-by: Miodrag Dinic <miodrag.dinic@imgtec.com>
    Signed-off-by: Aleksandar Markovic <aleksandar.markovic@imgtec.com>
    Signed-off-by: Goran Ferenc <goran.ferenc@imgtec.com>

---
+++ arch/mips/goldfish/goldfish-interrupt.c
@@ -0,0 +1,170 @@
+/* arch/mips/mach-goldfish/goldfish-platform.c
+ *
+ * Copyright (C) 2007 Google, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
+#include <linux/platform_device.h>
+#include <linux/delay.h>
+#include <linux/irqchip.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
+#include <linux/of_irq.h>
+
+#include <mach/hardware.h>
+#include <mach/irq.h>
+#include <asm/io.h>
+#include <asm/irq_cpu.h>
+#include <asm/setup.h>
+
+#define GOLDFISH_PIC_STATUS       0x00 // number of pending interrupts
+#define GOLDFISH_PIC_NUMBER       0x04
+#define GOLDFISH_PIC_DISABLE_ALL  0x08
+#define GOLDFISH_PIC_DISABLE      0x0c
+#define GOLDFISH_PIC_ENABLE       0x10
+
+static struct irq_domain *goldfish_pic_domain;
+static void __iomem *goldfish_pic_base;
+
+void goldfish_mask_irq(struct irq_data *d)
+{
+	writel(d->irq-GOLDFISH_IRQ_BASE,
+	       goldfish_pic_base + GOLDFISH_PIC_DISABLE);
+}
+
+void goldfish_unmask_irq(struct irq_data *d)
+{
+	writel(d->irq-GOLDFISH_IRQ_BASE,
+	       goldfish_pic_base + GOLDFISH_PIC_ENABLE);
+}
+
+static struct irq_chip goldfish_irq_chip = {
+	.name	= "goldfish",
+	.irq_mask	= goldfish_mask_irq,
+	.irq_mask_ack = goldfish_mask_irq,
+	.irq_unmask = goldfish_unmask_irq,
+};
+
+void goldfish_irq_dispatch(void)
+{
+	uint32_t irq;
+	/*
+	 * Disable all interrupt sources
+	 */
+	irq = readl(goldfish_pic_base + GOLDFISH_PIC_NUMBER);
+	do_IRQ(GOLDFISH_IRQ_BASE+irq);
+}
+
+void goldfish_fiq_dispatch(void)
+{
+	panic("goldfish_fiq_dispatch");
+}
+
+asmlinkage void plat_irq_dispatch(void)
+{
+	unsigned int pending = read_c0_cause() & read_c0_status() & ST0_IM;
+
+	if (pending & CAUSEF_IP2)
+		goldfish_irq_dispatch();
+	else if (pending & CAUSEF_IP3)
+		goldfish_fiq_dispatch();
+	else if (pending & CAUSEF_IP7)
+		do_IRQ(MIPS_CPU_IRQ_BASE + 7);
+	else
+		spurious_interrupt();
+}
+
+static struct irqaction cascade = {
+	.handler	= no_action,
+	.flags      = IRQF_NO_THREAD,
+	.name		= "cascade",
+};
+
+static void mips_timer_dispatch(void)
+{
+	do_IRQ(MIPS_CPU_IRQ_BASE + MIPS_CPU_IRQ_COMPARE);
+}
+
+static int goldfish_pic_map(struct irq_domain *d, unsigned int irq,
+				irq_hw_number_t hw)
+{
+	struct irq_chip *chip = &goldfish_irq_chip;
+
+	if (hw < GOLDFISH_IRQ_BASE)
+		return 0;
+
+	irq_set_chip_and_handler(hw, chip, handle_level_irq);
+
+	return 0;
+}
+
+static const struct irq_domain_ops irq_domain_ops = {
+	.xlate = irq_domain_xlate_onetwocell,
+	.map = goldfish_pic_map,
+};
+
+int __init goldfish_pic_of_init(struct device_node *node,
+				struct device_node *parent)
+{
+	struct resource res;
+
+	if (of_address_to_resource(node, 0, &res))
+		panic("Failed to get icu memory range");
+
+	if (request_mem_region(res.start, resource_size(&res),
+				res.name) < 0)
+		pr_err("Failed to request icu memory");
+
+	goldfish_pic_base = ioremap_nocache(res.start, resource_size(&res));
+
+	if (!goldfish_pic_base)
+		panic("Failed to remap icu memory");
+
+	/*
+	 * Disable all interrupt sources
+	 */
+	writel(1, goldfish_pic_base + GOLDFISH_PIC_DISABLE_ALL);
+
+	mips_cpu_irq_init();
+
+	if (cpu_has_vint) {
+		pr_info("Setting up vectored interrupts\n");
+		set_vi_handler(MIPS_CPU_IRQ_PIC, goldfish_irq_dispatch);
+		set_vi_handler(MIPS_CPU_IRQ_PIC, goldfish_fiq_dispatch);
+	}
+
+	setup_irq(MIPS_CPU_IRQ_BASE+MIPS_CPU_IRQ_PIC, &cascade);
+	setup_irq(MIPS_CPU_IRQ_BASE+MIPS_CPU_IRQ_FIQ, &cascade);
+
+	if (cpu_has_vint)
+		set_vi_handler(MIPS_CPU_IRQ_COMPARE, mips_timer_dispatch);
+
+	goldfish_pic_domain = irq_domain_add_linear(node, NR_IRQS,
+						&irq_domain_ops, 0);
+
+	return 0;
+}
+
+static const struct of_device_id of_irq_ids[] __initconst = {
+	{ .compatible = "google,goldfish-pic", .data = goldfish_pic_of_init },
+	{ .compatible = "generic,goldfish-pic", .data = goldfish_pic_of_init },
+	{},
+};
+
+void __init arch_init_irq(void)
+{
+	of_irq_init(of_irq_ids);
+}
---
+++ arch/mips/goldfish/goldfish-platform.c
@@ -0,0 +1,116 @@
+/* arch/mips/mach-goldfish/goldfish-platform.c
+ *
+ * Copyright (C) 2007 Google, Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
+#include <linux/platform_device.h>
+#include <linux/delay.h>
+#include <linux/of_platform.h>
+#include <linux/of_fdt.h>
+#include <linux/libfdt.h>
+
+#include <linux/bootmem.h>
+
+#include <mach/hardware.h>
+#include <mach/irq.h>
+#include <asm/io.h>
+#include <asm/bootinfo.h>
+#include <asm/mips-boards/generic.h>
+#include <asm/prom.h>
+
+#define HIGHMEM_OFFSET (0x20000000)
+
+void __init prom_init(void)
+{
+	char *cmdline = (char *)fw_arg0;
+
+	strcpy(arcs_cmdline, cmdline);
+}
+
+void prom_free_prom_memory(void)
+{
+}
+
+#ifdef CONFIG_64BIT
+#define GOLDFISH_TTY_PUT_CHAR (*(volatile unsigned int *)0xffffffffbf002000)
+#else
+#define GOLDFISH_TTY_PUT_CHAR (*(volatile unsigned int *)0xbf002000)
+#endif
+
+void prom_putchar(int c)
+{
+	GOLDFISH_TTY_PUT_CHAR = c;
+}
+
+const char *get_system_type(void)
+{
+	return "MIPS-Goldfish";
+}
+
+void __init plat_mem_setup(void)
+{
+#if defined(CONFIG_MIPS_RAW_APPENDED_DTB) && \
+	defined(CONFIG_MIPS_DTB_ALTERNATIVE_POSITION)
+	pr_info("Try setup using Appended DTB\n");
+	pr_info("DTB address: %p\n", (void *)fw_passed_dtb);
+	/*
+	 * This causes the "memory" node to be
+	 * parsed resulting in our memory appearing
+	 */
+	__dt_setup_arch(fw_passed_dtb);
+#else
+	/*
+	 * Load the built-in device tree.
+	 */
+	unsigned int ramsize = fw_arg1;
+	unsigned int highmem_ramsize = fw_arg2;
+
+	/*
+	 * Since RAM size is adjustable property in QEMU
+	 * do not rely on the built-in device tree information
+	 * for setting up memory, do it according to information
+	 * passed through arguments fw_arg1 & fw_arg2.
+	 */
+	add_memory_region(0x0, ramsize, BOOT_MEM_RAM);
+	if (highmem_ramsize)
+		add_memory_region(HIGHMEM_OFFSET, highmem_ramsize,
+					BOOT_MEM_RAM);
+
+	pr_info("Try setup using Built-in DTB\n");
+	__dt_setup_arch(__dtb_start);
+#endif
+}
+
+void __init device_tree_init(void)
+{
+	unflatten_and_copy_device_tree();
+}
+
+static const struct of_device_id goldfish_ids[] __initconst = {
+	{ .compatible = "simple-bus", },
+	{},
+};
+
+int __init plat_of_setup(void)
+{
+	if (!of_have_populated_dt())
+		panic("device tree not present");
+
+	return of_platform_populate(NULL, goldfish_ids, NULL, NULL);
+}
+
+arch_initcall(plat_of_setup);
